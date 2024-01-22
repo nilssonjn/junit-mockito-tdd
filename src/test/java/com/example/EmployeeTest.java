b20e0d8a36d7c620e9915ec107b9dded513b5e5c
jnnilsson <jn.jensnilsson@gmail.com>
2024-01-22 20:06:45 +0100

    add test verify employee initialization, update pom.xml

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    @Test
+    @DisplayName("Verify employee initialization")
+    void verifyEmployeeInitialization() {
+        Employee employee = new Employee("John", 15000);
+
+        assertThat(employee.getId()).isEqualTo("John");
+        assertThat(employee.getSalary()).isEqualTo(15000);
+    }
 }
