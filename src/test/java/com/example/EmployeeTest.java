97abc756f18adeca0ae6f94cd9829b62d72b92bd
jnnilsson <jn.jensnilsson@gmail.com>
2024-01-22 20:37:46 +0100

    add test isPaid and setPaid returns correct value

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,10 +32,27 @@ class EmployeeTest {
     @DisplayName("Assure setSalary updates a new salary")
     void assureSetSalaryUpdatesANewSalary() {
         Employee employee = new Employee("John", 15000);
-
         double updatedSalary = 20000.0;
         employee.setSalary(updatedSalary);
 
         assertThat(employee.getSalary()).isEqualTo(20000.0);
+    }
+
+    @Test
+    @DisplayName("Assure isPaid returns correct value")
+    void assureIsPaidReturnsCorrectValue() {
+        Employee employee = new Employee("John", 20000.0);
+        employee.setPaid(true);
+
+        assertThat(employee.isPaid()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Assure setPaid returns correct value")
+    void assureSetPaidReturnsCorrectValue() {
+        Employee employee = new Employee("John", 20000.0);
+        employee.setPaid(false);
+
+        assertThat(employee.isPaid()).isEqualTo(false);
     }
 }
