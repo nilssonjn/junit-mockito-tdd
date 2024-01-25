bd5bd2d0bb78fa56e429f3a6536b33a8c3babfc9
jnnilsson <jn.jensnilsson@gmail.com>
2024-01-25 16:13:06 +0100

    add test for unknown amount numbers

--- src/main/java/com/example/uppgift2/StringCalculator.java
+++ src/main/java/com/example/uppgift2/StringCalculator.java
@@ -3,7 +3,17 @@ package com.example.uppgift2;
 public class StringCalculator {
 
     public int add(String numbers) {
+        if (numbers.length() > 1) return splitNumbers(numbers);
         if (numbers.length() == 1) return Integer.parseInt(numbers);
         return 0;
+    }
+
+    private int splitNumbers(String numbers) {
+        String[] splitNumbers = numbers.split(",");
+        int sum = 0;
+        for (String number : splitNumbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
--- src/test/java/com/example/uppgift2/StringCalculatorTest.java
+++ src/test/java/com/example/uppgift2/StringCalculatorTest.java
@@ -18,14 +18,37 @@ class StringCalculatorTest {
     @Test
     @DisplayName("Given empty string, return zero")
     void givenEmptyStringReturnZero() {
-
         assertThat(stringCalculator.add("")).isEqualTo(0);
     }
 
     @Test
     @DisplayName("Given 1 return 1")
     void given1Return1() {
-
         assertThat(stringCalculator.add("1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Given 1,2 return 3")
+    void given12ShouldReturn3() {
+        assertThat(stringCalculator.add("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Given 1,2,3,4,5,6 return 21")
+    void given123Return6() {
+        assertThat(stringCalculator.add("1,2,3,4,5,6")).isEqualTo(21);
+
+    }
+
+    @Test
+    @DisplayName("Given 4,4 returns 8")
+    void given44Returns8() {
+        assertThat(stringCalculator.add("4,4")).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("Given 50,50 returns 100")
+    void given5050Returns100() {
+        assertThat(stringCalculator.add("50,50")).isEqualTo(100);
     }
 }
