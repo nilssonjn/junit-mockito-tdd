183edfad3b0db79493c81ff090d6ceb50574976e
jnnilsson <jn.jensnilsson@gmail.com>
2024-01-29 16:19:58 +0100

    add test for given employee mock should throw exception, clean up

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -6,18 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeesTest {
@@ -45,10 +41,21 @@ class EmployeesTest {
         Employee employee = new Employee("1", 10000.00);
 
         when(employeeRepository.findAll()).thenReturn(List.of(employee));
-
         employees.payEmployees();
 
         assertThat(employee.isPaid()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Given employee mock should throw exception")
+    void givenEmployeeMockShouldThrowException() {
+        Employee employee = new Employee(null, 2);
+
+        when(employeeRepository.findAll()).thenReturn(List.of(employee));
+        doThrow(new RuntimeException("There was a problem paying")).when(bankService).pay(anyString(), anyDouble());
+        var result = employees.payEmployees();
+
+        assertThat(result).isEqualTo(0);
     }
 
     @Test
